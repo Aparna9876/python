d6edfb26bcd195d3a82bbe2ae3bb0a7e577e96b9
Aparna9876 <58392587+Aparna9876@users.noreply.github.com>
2020-01-13 15:14:28 +0530

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,11 @@
+def fizzbuzz(r):
+    for i in range(1,r):
+        if(i%3==0 and i%5==0):
+            print(str(i)+"=FizzBuzz")
+        elif(i%3==0):
+            print(str(i)+"=Fizz")
+        elif(i%5==0):
+            print(str(i)+"=Buzz")
+        else:
+            print(str(i))
+                
